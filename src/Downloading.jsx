4b54980f5e1f94e6f64822cd29610e8a9c44c1d2
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-12-22 13:41:26 +0500

    some code

--- src/Downloading.jsx
+++ src/Downloading.jsx
@@ -12,7 +12,7 @@ export default function OpenPdf() {
       setTimeout(() => {
         setLoader(false)
         setDownloadBtn(true)
-      }, 4000);
+      }, 2000);
     } catch (error) {
       setDownloadBtn(true)
       setLoader(false)
@@ -21,9 +21,9 @@ export default function OpenPdf() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen mx-8">
-      <a href={myPdf} target="_blank" rel="noopener noreferrer" className="px-12 bg-blue-800 py-4 text-white rounded-xl hover:bg-blue-900 active:bg-blue-950">Download PDF</a>
+      
      {
-      loader && (
+      loader ? (
         <div
   class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] text-pink-700"
   role="status">
@@ -31,6 +31,9 @@ export default function OpenPdf() {
     class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
     >Loading...</span>
 </div>
+      ) :
+      (
+        <a href={myPdf} target="_blank" rel="noopener noreferrer" className="px-12 bg-blue-800 py-4 text-white rounded-xl hover:bg-blue-900 active:bg-blue-950">Download PDF</a>
       )
      }
     </div>
