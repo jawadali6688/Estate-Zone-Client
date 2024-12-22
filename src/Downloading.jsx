922dc05fbdf05113d0cd0baba3eb835baa21ef0c
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-12-22 13:37:13 +0500

    some code

--- src/Downloading.jsx
+++ src/Downloading.jsx
@@ -9,7 +9,7 @@ export default function OpenPdf() {
       window.open(myPdf, "_self");
       setTimeout(() => {
         setLoader(false)
-      }, 2000);
+      }, 4000);
     } catch (error) {
       setLoader(false)
     }
@@ -21,7 +21,7 @@ export default function OpenPdf() {
      {
       loader && (
         <div
-  class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+  class="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] text-pink-700"
   role="status">
   <span
     class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
