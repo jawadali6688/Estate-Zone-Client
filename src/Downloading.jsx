d2d7046a5a399aa0a00470322016af01f5e0ad92
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-12-22 12:21:49 +0500

    some code

--- src/Downloading.jsx
+++ src/Downloading.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import myPdf from "./tuv card new final version (2).pdf"
+import React, { useEffect } from "react";
+import myPdf from "./id_card.pdf";
+
 export default function Downloading() {
+  useEffect(() => {
+    const link = document.createElement("a");
+    link.href = myPdf; // Set the PDF file URL
+    link.download = "id_card.pdf"; // Set the default filename for the download
+    link.click(); // Programmatically trigger the click to start the download
+  }, []);
+
   return (
     <div>
-       <iframe
+      <iframe
         src={myPdf}
         title="PDF Viewer"
         className="border w-full h-[100vh]"
       />
     </div>
-  )
+  );
 }
