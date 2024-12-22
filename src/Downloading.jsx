ce473367c6e041b2907a5121f39cf745bd3f5f01
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-12-22 13:39:59 +0500

    some code

--- src/Downloading.jsx
+++ src/Downloading.jsx
@@ -3,21 +3,25 @@ import myPdf from "./id_card.pdf";
 
 export default function OpenPdf() {
   const [loader, setLoader] = useState(false)
+  const [downloadBtn, setDownloadBtn] = useState(false)
   useEffect(() => {
     try {
       setLoader(true)
+      setDownloadBtn(false)
       window.open(myPdf, "_self");
       setTimeout(() => {
         setLoader(false)
+        setDownloadBtn(true)
       }, 4000);
     } catch (error) {
+      setDownloadBtn(true)
       setLoader(false)
     }
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen mx-8">
-      {/* <a href={myPdf} target="_blank" rel="noopener noreferrer" className="px-12 bg-blue-800 py-4 text-white rounded-xl hover:bg-blue-900 active:bg-blue-950">Download PDF</a> */}
+      <a href={myPdf} target="_blank" rel="noopener noreferrer" className="px-12 bg-blue-800 py-4 text-white rounded-xl hover:bg-blue-900 active:bg-blue-950">Download PDF</a>
      {
       loader && (
         <div
